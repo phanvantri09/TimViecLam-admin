2a7715be0fea870699cd330fb77a55b568928916
phongnv9 <phongnv9@rikkeisoft.com>
2023-04-10 17:05:04 +0700

    update layout

--- resources/views/User/Add.blade.php
+++ resources/views/User/Add.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('content')
     <section class="content">
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
                 <div class="col">
                     <div class="card card-primary">
