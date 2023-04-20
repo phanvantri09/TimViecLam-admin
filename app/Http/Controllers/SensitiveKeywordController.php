85d93b12b714fe4b052477e882c963bb49e35814
tuan <duongvantuan1503@gmail.com>
2023-04-20 16:27:04 +0700

    done CRUD keyword

--- app/Http/Controllers/SensitiveKeywordController.php
+++ app/Http/Controllers/SensitiveKeywordController.php
@@ -1,12 +1,47 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\KeywordSensitive;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class SensitiveKeywordController extends Controller
 {
-   public function GetKeywordSensitive(){
-    $keyWords = DB::table('keyword_sensitive')->pluck('name')->toArray();
-    return view('admin.keywordSensitive', compact(['keyWords']));
+   public function GetKeywordSensitive()
+   {
+      $keyWords = DB::table('keyword_sensitive')->pluck('name')->toArray();
+      return view('admin.keywordSensitive', compact(['keyWords']));
    }
+   public function ListKeywordSensitive()
+   {
+      $key = DB::table('keyword_sensitive')->get();
+      return view('admin.listKeywordSensitive', compact(['key']));
+   }
+
+   public function AddKeywordSensitive(Request $request)
+   {
+      $keyword = $request->input('name');
+      // Kiểm tra từ khóa đã tồn tại trong DB hay chưa
+      $existingKeyword = DB::table('keyword_sensitive')->where('name', $keyword)->first();
+      if ($existingKeyword) {
+         // Nếu từ khóa đã tồn tại, thông báo lỗi và không lưu vào DB
+         return redirect()->back()->with('error', 'Từ khóa này đã tồn tại.');
+      } else {
+         // Nếu từ khóa chưa tồn tại, lưu vào DB
+         $newKeyword = new KeywordSensitive;
+         $newKeyword->name = $keyword;
+         $newKeyword->save();
+
+         return redirect()->back()->with('success', 'Thêm từ khóa bị cấm thành công.');
+      }
+   }
+   public function DeleteKeywordSensitive($id)
+   {
+      $keyword = KeywordSensitive::find($id);
+      $keyword->delete();
+      return back()->with('success', 'Xóa từ cấm thành công');
+
+   }
+
 }
---
+++ resources/views/admin/listKeyWordSensitive.blade.php
@@ -0,0 +1,161 @@
+@extends('layouts.master')
+@section('content')
+    @php
+        use App\Helpers\CommonFunction;
+        use App\Helpers\CommonVaiation;
+    @endphp
+<section class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Danh sách các từ khóa bị cấm</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                                <div class="row">
+                                    <div class="col-sm-12 col-md-12">
+                                        <div class="dt-buttons btn-group flex-wrap">
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-12 col-md-6">
+                                        <div id="example1_filter" class="dataTables_filter">
+                                            <label>Search: <input type="search" class="form-control form-control-sm"
+                                                    placeholder="" aria-controls="example1">
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <h3>Thêm từ khóa bị cấm</h3>
+                                        <form action="{{ route('SensitiveKeyword.addKeywordSensitive') }}" method="POST">
+                                            @csrf
+                                            <div class="form-group">
+                                                <label for="name">Tên từ khóa:</label>
+                                                <input type="text" name="name" class="form-control" id="name" placeholder="Nhập tên từ khóa">
+                                            </div>
+                                            <button type="submit" class="btn btn-primary">Thêm mới</button>
+                                        </form>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <table id="example1"
+                                            class="table table-bordered table-striped dataTable dtr-inline"
+                                            aria-describedby="example1_info">
+                                            <thead>
+                                                <tr>
+                                                <th class="sorting sorting_asc" tabindex="0" aria-controls="example1"
+                                                        rowspan="1" colspan="1" aria-sort="ascending"
+                                                        aria-label="Rendering engine: activate to sort column descending">
+                                                        Id
+                                                    </th>
+                                                    <th class="sorting sorting_asc" tabindex="0" aria-controls="example1"
+                                                        rowspan="1" colspan="1" aria-sort="ascending"
+                                                        aria-label="Rendering engine: activate to sort column descending">
+                                                        Tên
+                                                    </th>
+                                                    <th class="sorting" tabindex="0" aria-controls="example1"
+                                                        rowspan="1" colspan="1"
+                                                        aria-label="CSS grade: activate to sort column ascending">Hành động
+                                                    </th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($key as $key)
+                                                    <tr class="odd">
+                                                    <td class="dtr-control sorting_1" tabindex="0">
+                                                            {{ $key->id }}
+                                                        </td>
+                                                        <td class="dtr-control sorting_1" tabindex="0">
+                                                            {{ $key->name }}
+                                                        </td>
+                                                        <td>
+                                                            <form action="{{ route('SensitiveKeyword.deleteKeyword',['id' => $key->id]) }}" method="GET">
+                                                                @csrf
+                                                                <button type="submit" class="btn btn-app"><i class="fas fa-edit"></i> Xóa</button>
+                                                            </form>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-12 col-md-5">
+                                        <div class="dataTables_info" id="example1_info" role="status"
+                                            aria-live="polite">
+                                            Showing 1 to 10 of 57 entries</div>
+                                    </div>
+                                    <div class="col-sm-12 col-md-7">
+                                        <div class="dataTables_paginate paging_simple_numbers" id="example1_paginate">
+                                            <ul class="pagination">
+                                                <li class="paginate_button page-item previous disabled"
+                                                    id="example1_previous">
+                                                    <a href="#" aria-controls="example1" data-dt-idx="0"
+                                                        tabindex="0" class="page-link">Previous</a>
+                                                </li>
+                                                <li class="paginate_button page-item active">
+                                                    <a href="#" aria-controls="example1" data-dt-idx="1"
+                                                        tabindex="0" class="page-link">1</a>
+                                                </li>
+                                                <li class="paginate_button page-item ">
+                                                    <a href="#" aria-controls="example1" data-dt-idx="2"
+                                                        tabindex="0" class="page-link">2</a>
+                                                </li>
+                                                <li class="paginate_button page-item ">
+                                                    <a href="#" aria-controls="example1" data-dt-idx="3"
+                                                        tabindex="0" class="page-link">3</a>
+                                                </li>
+                                                <li class="paginate_button page-item ">
+                                                    <a href="#" aria-controls="example1" data-dt-idx="4"
+                                                        tabindex="0" class="page-link">4</a>
+                                                </li>
+                                                <li class="paginate_button page-item ">
+                                                    <a href="#" aria-controls="example1" data-dt-idx="5"
+                                                        tabindex="0" class="page-link">5</a>
+                                                </li>
+                                                <li class="paginate_button page-item ">
+                                                    <a href="#" aria-controls="example1" data-dt-idx="6"
+                                                        tabindex="0" class="page-link">6</a>
+                                                </li>
+                                                <li class="paginate_button page-item next" id="example1_next">
+                                                    <a href="#" aria-controls="example1" data-dt-idx="7"
+                                                        tabindex="0" class="page-link">Next</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script>
+        $('.changheStatus').change(function(){
+            let id = $(this).attr('id');
+            let status = $(this).val();
+            console.log(id, status);
+            $.ajax({
+                    type: 'GET',
+                    url: "{{route('Job.Status')}}",
+                    data: {
+                        id : id ,
+                        status : status
+                    },
+                    success: function(data) {
+                        alert('oke');
+                    }
+                });
+        })
+    </script>
+  
+@endsection
