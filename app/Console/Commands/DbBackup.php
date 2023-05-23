c689a76477e3aec77edaf8e333c976821efe9c22
Tri PV <hulk09022000@gmail.com>
2023-05-23 11:23:35 +0700

    backup db

---
+++ app/Console/Commands/DbBackup.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Carbon\Carbon;
+
+class DbBackup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:backup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create Database Backup';
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $filename = "backup-" . Carbon::now()->format('Y-m-d') . ".gz";
+        $storageAt = storage_path() . "/app/backup/";
+        if(!File::exists($storageAt)) {
+            File::makeDirectory($storageAt, 0755, true, true);
+        }
+        $command = "mysqldump --user=" . env('DB_USERNAME') ." --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . "  | gzip > " . storage_path() . $storageAt . $filename;
+        $returnVar = NULL;
+        $output  = NULL;
+        // dd($command );
+        exec($command, $output, $returnVar);
+    }
+}
