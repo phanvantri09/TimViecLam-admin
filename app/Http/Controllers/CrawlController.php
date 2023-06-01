906bc3efadfdd16fe4237ca8c90c7682781ba3db
phongnv9 <phongnv9@rikkeisoft.com>
2023-06-01 17:31:38 +0700

    update

--- app/Http/Controllers/CrawlController.php
+++ app/Http/Controllers/CrawlController.php
@@ -9,7 +9,7 @@ class CrawlController extends Controller
 {
     public function CrawlPostJob(){
         $arrayLinks = [];
-        for ($i=1; $i <= 3; $i++) { 
+        for ($i=1; $i <= 1; $i++) { 
             $crawler = GoutteFacade::request('GET', 'https://vieclam24h.vn/tim-kiem-viec-lam-nhanh?page='.$i);
             $Links = $crawler->filter('div.rounded-sm a')->each(function ($node) {
                 return $node->attr("href");
@@ -23,17 +23,18 @@ class CrawlController extends Controller
                 if ( !empty($arrayLink)) {
                     foreach ($arrayLink as $key => $itemLink) {
                         $content = GoutteFacade::request('GET', 'https://vieclam24h.vn/ban-buon-ban-le-quan-ly-cua-hang/nhan-vien-telesales-thu-nhap-20-30tr-c6p73id200223028.html');
-                        $nameCompany = $content->filter('div.px-4 a h3')->each(function ($node) {
-                            return $node->text();
-                        });
-                        $reviewCompany = $content->filter('div.text-se-neutral-84')->each(function ($node) {
-                            return $node->text();
-                        });
+                        // $nameCompany = $content->filter('div.px-4 a h3')->each(function ($node) {
+                        //     return $node->text();
+                        // });
+                        // $reviewCompany = $content->filter('div.text-se-neutral-84')->each(function ($node) {
+                        //     return $node->text();
+                        // });
                         $main = $content->filter('main')->each(function ($node) {
                             return $node->text();
                         });
+                        $k = explode(' - ', $main[0]);
                     }
-                    dd($nameCompany, $reviewCompany, $main);
+                    dd($k, $main[0]);
                 }
             }
         }
